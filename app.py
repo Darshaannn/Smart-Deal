233c765b75151fc584b213bcdfbebe3c5491c050
Darshaannn <darshangadhave10@gmail.com>
2026-09-02 19:44:10 +0530

    Trying ti be constisentent

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import streamlit as st 
+import joblib
 
 st.title("Samrt Deal")
 
 st.write("AI-Assisted Used Car Price & Deal Intelligence")
 
+bundle=joblib.load("smartdeal_bunddle.joblib")
+
+model=bundle["model"]
+tolerance =model["tolerance "]
+features=bundle["features"]
